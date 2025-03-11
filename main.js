cf998fcf0ef288df265da429812462d4a93e735d
nesmanpro <nesmanpro@gmail.com>
2025-03-11 21:50:26 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,106 @@
+import { enjoyItems, learnItems, playItems } from "./src/mock/cardItems.js";
+
+
+const playContainer = document.querySelector('.play-cards-container');
+const learnContainer = document.querySelector('.learn-cards-container');
+const enjoyContainer = document.querySelector('.enjoy-cards-container');
+
+
+// playContainer.appendChild('Loading...');
+
+
+
+
+const cardLayout = (card) => {
+
+    const cardWrapper = document.createElement("a");
+    cardWrapper.classList.add("card-wrapper");
+    cardWrapper.href = "#";
+
+    // Contenedor interno
+    const cardInner = document.createElement("div");
+    cardInner.classList.add("card-inner");
+
+    // Imagen principal
+    const cardImage = document.createElement("img");
+    cardImage.classList.add("card-image");
+    cardImage.src = card.image;
+    cardImage.alt = card.title;
+
+    // Sección de texto
+    const cardText = document.createElement("div");
+    cardText.classList.add("card-text");
+
+    // Icono de la tarjeta
+    const cardIcon = document.createElement("img");
+    cardIcon.classList.add("card-icon");
+    cardIcon.src = card.image;
+    cardIcon.alt = card.title;
+
+    // Contenedor de información
+    const cardInfo = document.createElement("div");
+    cardInfo.classList.add("card-info");
+
+    // Título
+    const title = document.createElement("h3");
+    title.textContent = card.title;
+
+    // raiting
+    const cardRating = document.createElement("div");
+    const starWrapper = document.createElement("div");
+    cardRating.classList.add("card-rating");
+
+    for (let i = 0; i < card.rating; i++) {
+        const star = document.createElement("span");
+        star.textContent = "★";
+        starWrapper.appendChild(star);
+    }
+    cardRating.appendChild(starWrapper);
+
+    // Botón
+    const button = document.createElement("button");
+    button.textContent = "Get";
+
+    // Descripción
+    const cardDescription = document.createElement("p");
+    cardDescription.classList.add("card-description");
+    cardDescription.textContent = card.description;
+
+
+
+    // info block
+    cardInfo.appendChild(title);
+
+    if (card.description && card.rating) {
+
+        // raiting
+        cardRating.appendChild(button);
+        cardInfo.appendChild(cardRating);
+
+        // description
+        cardInfo.appendChild(cardDescription);
+        // icon
+        cardText.appendChild(cardIcon);
+    }
+
+    // text 
+    cardText.appendChild(cardInfo);
+
+    // card inner
+    cardInner.appendChild(cardImage);
+    cardInner.appendChild(cardText);
+    cardWrapper.appendChild(cardInner);
+
+    return cardWrapper;
+};
+
+
+const renderCards = (containr, items) => {
+    items.forEach((card) => {
+        containr.appendChild(cardLayout(card));
+    });
+};
+
+renderCards(playContainer, playItems);
+renderCards(learnContainer, learnItems);
+renderCards(enjoyContainer, enjoyItems);
---
+++ src/mock/cardItems.js
@@ -0,0 +1,62 @@
+
+const playItems = [
+    {
+        title: "Disney XD Villains Ahead!",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        rating: 5,
+        image: "./src/assets/game01.png"
+    },
+    {
+        title: "Monster Hunting!",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        rating: 4,
+        image: "./src/assets/game02.jpg"
+    },
+    {
+        title: "The Simpsons Matching Pairs",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        rating: 3,
+        image: "./src/assets/game03.png"
+    }
+];
+
+const learnItems = [
+    {
+        title: "Teach Duolingo Language",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        rating: 2,
+        image: "./src/assets/app01.png"
+    },
+    {
+        title: "English Vocabulary",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        rating: 5,
+        image: "./src/assets/app02.png"
+    },
+    {
+        title: "Answer the Question",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        rating: 4,
+        image: "./src/assets/app03.png"
+    }
+];
+
+
+const enjoyItems = [
+    {
+        title: "Teach Duolingo Language",
+        image: "./src/assets/video01.jpg"
+    },
+    {
+        title: "English Vocabulary",
+        image: "./src/assets/video02.jpg"
+    },
+    {
+        title: "Answer the Question",
+        image: "./src/assets/video03.jpg"
+    }
+];
+
+
+export { enjoyItems, learnItems, playItems };
+
